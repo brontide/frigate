c818ab4586fc9d5c87b1a3efceb56d553190b12a
Eric W <ericew@gmail.com>
2025-03-14 13:54:43 -0400

    add mog2 to factory

--- frigate/motion/factory.py
+++ frigate/motion/factory.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from frigate.config import MotionConfig
 
 from .improved_motion import ImprovedMotionDetector
+from .mog2_motion import MoG2Detector 
 
 
 # Add a facotry function that can instantiate different motion detectors based on configs
@@ -15,6 +16,7 @@ def motionFactory(
 ):
     methods = {
         config.method.improved: ImprovedMotionDetector,
+        config.method.mog2: MoG2Detector,
     }
     # return the motion class from the array above, defaulting to
     # improved_motion if there are any issues
